8e66594ff757fca921c53b02eaac02cf88508a22
Roland Palmer <roland.palmer@int-group.de>
2017-10-09 11:05:25 +0200

    change temp.php

--- Java_Client/Wetterstation_Client/src/de/rolandpalmer/wetterstationclient/WetterstationClient.java
+++ Java_Client/Wetterstation_Client/src/de/rolandpalmer/wetterstationclient/WetterstationClient.java
@@ -4,25 +4,22 @@ import java.io.IOException;
 
 public class WetterstationClient {
 
-	static String path = "c:\\temp\\test.txtt";
+	static String path = "c:\\temp\\test.txt";
 	
 	public static void main(String[] args) throws IOException {
 
-		// read file
-		FileContentReader rfc = new FileContentReader(path);
-		String str = rfc.ReadFileContent();
-		System.out.println(str);
-		
-		
+		// get temperature
 		TemperatureReader rt = new TemperatureReader(path);
 		String temp = rt.GetTemperature();
 		System.out.println(temp);
 		
-		//String strContent = rfc(path);
-		
-		// get temperature
-		
 		// save to database
+		// fields: - id
+		//         - date_time
+		//         - temperature
+		//         - committed
+		
+		// 
 		
 		// send to server
 		
--- webdoc/temp.php
+++ webdoc/temp.php
@@ -1,5 +1,28 @@
+<html>
+<title> FTIT17 - Wetterstation Übermittlung</title>
+<body>
 <?php
-	if(isset($_GET['stationID'])){
-		echo 'Station: ' . htmlspecialchars($_GET['stationID']) . "!";
+	foreach($_GET as $key 0> $value){
+		echo $key . ' : ' . $value;
 	}
-?>
+	
+	if(empty($_GET["temp"] ) {
+		echo "Parameter 'temp' fehlt."
+		echo "Übermittlung fehlerhaft!";
+	}
+	if(empty(empty($_GET["dbg"] ) {
+		echo "Parameter 'dbg' fehlt."
+		echo "Übermittlung fehlerhaft!";
+	}
+	if(empty($_GET["token"] ) {
+		echo "Parameter 'token' fehlt."
+		echo "Übermittlung fehlerhaft!";
+	}
+	if(empty($_GET["timestamp"] ) {
+		echo "Parameter 'timestamp' fehlt."
+		echo "Übermittlung fehlerhaft!";
+	}
+	# value noch prüfen ob valide
+	
+	?>
+</body>
