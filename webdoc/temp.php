e6d6df688aaac83c44ce9e13060c926ee0f25847
Roland Palmer <roland.palmer@int-group.de>
2017-10-09 11:33:05 +0200

    change temp.php

--- webdoc/temp.php
+++ webdoc/temp.php
@@ -4,24 +4,24 @@
 <h1>FTIT17 - Wetterstation Übermittlung</h1>
 <?php
 	foreach($_GET as $key => $value){
-		echo $key . ' : ' . $value;
+		echo "<p>" . $key . ' : ' . $value . "</p>";
 	}
 	
-	if(empty($_GET["temp"] ) {
-		echo "Parameter 'temp' fehlt."
-		echo "Übermittlung fehlerhaft!";
+	if(empty($_GET["temp"] )) {
+		echo "<p><div>Parameter 'temp' fehlt.</div>";
+		echo "<div>Übermittlung fehlerhaft!</div></p>";
 	}
-	if(empty(empty($_GET["dbg"] ) {
-		echo "Parameter 'dbg' fehlt."
-		echo "Übermittlung fehlerhaft!";
+	if(empty($_GET["dbg"] )) {
+		echo "<p><div>Parameter 'dbg' fehlt.</div>";
+		echo "<div>Übermittlung fehlerhaft!</div></p>";
 	}
-	if(empty($_GET["token"] ) {
-		echo "Parameter 'token' fehlt."
-		echo "Übermittlung fehlerhaft!";
+	if(empty($_GET["token"] )) {
+		echo "<p><div>Parameter 'token' fehlt.</div>";
+		echo "<div>Übermittlung fehlerhaft!</div></p>";
 	}
-	if(empty($_GET["timestamp"] ) {
-		echo "Parameter 'timestamp' fehlt."
-		echo "Übermittlung fehlerhaft!";
+	if(empty($_GET["timestamp"] )) {
+		echo "<p><div>Parameter 'timestamp' fehlt.</div>";
+		echo "<div>Übermittlung fehlerhaft!</div></p>";
 	}
 	# value noch prüfen ob valide
 	
